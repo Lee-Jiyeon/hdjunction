20afd665bf4f05629cab4840c80fbc8c1dc3e34d
jylee <sirshe805@gmail.com>
2024-04-19 17:16:17 +0900

    init 프로젝트 생성

---
+++ src/main/java/com/hdjunction/server/docapi/DocapiApplication.java
@@ -0,0 +1,13 @@
+package com.hdjunction.server.docapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdjunction/server/docapi/DocapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdjunction.server.docapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
